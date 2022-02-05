65c243c3244aaef9b8adef29feda8efed763ccab
Atsushi-110925 <76760276+Atsushi-110925@users.noreply.github.com>
2022-02-06 04:07:42 +0900

    Update jquery.inview_set.js

--- js/jquery.inview_set.js
+++ js/jquery.inview_set.js
@@ -4,11 +4,6 @@ $('main h2, .blur').not('.simple').on('inview', function() {
 	$(this).addClass('blurstyle');
 });
 
-//upスタイルが画面内にきたら、スタイルupstyleを適用する
-$('.up').on('inview', function() {
-	$(this).addClass('upstyle');
-});
-
 //leftスタイルが画面内にきたら、スタイルleftstyleを適用する
 $('.left').on('inview', function() {
 	$(this).addClass('leftstyle');
@@ -17,14 +12,4 @@ $('.left').on('inview', function() {
 //rightスタイルが画面内にきたら、スタイルrightstyleを適用する
 $('.right').on('inview', function() {
 	$(this).addClass('rightstyle');
-});
-
-//transform1スタイルが画面内にきたら、スタイルtransform1styleを適用する
-$('.transform1').on('inview', function() {
-	$(this).addClass('transform1style');
-});
-
-//transform2スタイルが画面内にきたら、スタイルtransform2styleを適用する
-$('.transform2').on('inview', function() {
-	$(this).addClass('transform2style');
 });
