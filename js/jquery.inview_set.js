b79a4efd7a53d7abbe97e1ea23264b0f3f4ca8c9
Atsushi-110925 <76760276+Atsushi-110925@users.noreply.github.com>
2022-02-06 04:51:27 +0900

    Update jquery.inview_set.js

--- js/jquery.inview_set.js
+++ js/jquery.inview_set.js
@@ -1,7 +1,11 @@
-
 //main h2またはblurクラスがついた要素が画面内にきたら、スタイルblurstyleを適用する
 $('main h2, .blur').not('.simple').on('inview', function() {
 	$(this).addClass('blurstyle');
+});
+
+//upスタイルが画面内にきたら、スタイルupstyleを適用する
+$('.up').on('inview', function() {
+	$(this).addClass('upstyle');
 });
 
 //leftスタイルが画面内にきたら、スタイルleftstyleを適用する
@@ -13,3 +17,4 @@ $('.left').on('inview', function() {
 $('.right').on('inview', function() {
 	$(this).addClass('rightstyle');
 });
+
