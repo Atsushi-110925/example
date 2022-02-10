bcc74689f81e0699e8e48b19b4cbf9723a169115
Atsushi-110925 <76760276+Atsushi-110925@users.noreply.github.com>
2022-02-11 01:28:51 +0900

    Update jquery.inview_set.js

--- js/jquery.inview_set.js
+++ js/jquery.inview_set.js
@@ -56,3 +56,18 @@ function delayScrollAnime() {
   $(window).on('load', function(){
     delayScrollAnime();
   });
+
+
+ $('flex box a').click(function() {
+        var imgSrc = $(this).children().attr('src');
+        $('.bigimg').children().attr('src', imgSrc);
+        $('.modal').fadeIn();
+        $('body,html').css('overflow-y', 'hidden');
+        return false
+      });
+
+$('.close-btn').click(function() {
+        $('.modal').fadeOut();
+        $('body,html').css('overflow-y', 'visible');
+        return false
+      });
